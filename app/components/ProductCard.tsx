2c85326ff523fe12591968c0df36a13c6d6b0eb2
kk1528 <2020UEC1528@mnit.ac.in>
2024-10-02 21:18:48 +0530

    finished learning NEXTJS

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import AddToCart from './AddToCart'
+// import styles from './ProductCard.module.css';
+
+
+
+const ProductCard = () => {
+  return (
+    <div className='p-4 my-3 bg-sky-400'>
+      <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductCard
